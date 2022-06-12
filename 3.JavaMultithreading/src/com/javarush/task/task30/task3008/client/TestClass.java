a4cb44b7c0009e31882dc5d1b3147116f15b7bbe
Justwantedtosavemyawfulcode <youjustalittlechicken@yandex.ru>
2022-06-12 20:09:33 +0300

    did something at home

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/TestClass.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/TestClass.java
@@ -1,5 +1,7 @@
 package com.javarush.task.task30.task3008.client;
 
 public class TestClass {
-
+    public static void main(String[] args) {
+        System.out.println("Do something at home");
+    }
 }
