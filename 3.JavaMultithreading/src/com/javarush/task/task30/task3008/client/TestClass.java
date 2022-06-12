520d6bf4e544982dc41f34b2cb4be373231ca263
Justwantedtosavemyawfulcode <youjustalittlechicken@yandex.ru>
2022-06-12 20:13:55 +0300

    changed gitignore file on laptop

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/TestClass.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/client/TestClass.java
@@ -2,6 +2,6 @@ package com.javarush.task.task30.task3008.client;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println("Do something at home");
+        System.out.println("fuck out.log");
     }
 }
