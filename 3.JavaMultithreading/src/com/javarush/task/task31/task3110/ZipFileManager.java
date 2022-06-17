c85a5a5d3e7348e5233933b912b1ea10870ad55b
Justwantedtosavemyawfulcode <youjustalittlechicken@yandex.ru>
2022-06-17 20:21:38 +0300

    did something at home

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,6 +1,10 @@
 package com.javarush.task.task31.task3110;
 
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;
@@ -10,7 +14,19 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+        try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
+        ZipEntry zipEntry = new ZipEntry(source.getFileName().toString());
+        zipOutputStream.putNextEntry(zipEntry);
 
+        try (InputStream fileInputStream = Files.newInputStream(source)) {
+            byte[] inBytes = new byte[1000];
+            while (fileInputStream.available() > 0) {
+                int count = fileInputStream.read(inBytes);
+                zipOutputStream.write(inBytes, 0, count);
+            }
+        }
+        zipOutputStream.closeEntry();
+        }
     }
 
 }
