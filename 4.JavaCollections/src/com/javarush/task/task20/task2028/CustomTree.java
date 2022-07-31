b65f88056a0296edae1de7991f2f8d43ee5e9880
Justwantedtosavemyawfulcode <youjustalittlechicken@yandex.ru>
2022-07-31 17:08:12 +0300

    did something at home

--- 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
+++ 4.JavaCollections/src/com/javarush/task/task20/task2028/CustomTree.java
@@ -51,4 +51,23 @@ public class CustomTree extends AbstractList<String> implements Cloneable, Seria
     protected void removeRange(int fromIndex, int toIndex) {
         throw new UnsupportedOperationException();
     }
+
+    static class Entry<T> implements Serializable {
+        String elementName;
+        boolean availableToAddLeftChildren;
+        boolean availableToAddRightChildren;
+        Entry<T> parent;
+        Entry<T> leftChild;
+        Entry<T> rightChild;
+
+        public Entry(String elementName) {
+            this.elementName = elementName;
+            availableToAddLeftChildren = true;
+            availableToAddRightChildren = true;
+        }
+
+        public boolean isAvailableToAddChildren() {
+            return availableToAddLeftChildren || availableToAddRightChildren;
+        }
+    }
 }
