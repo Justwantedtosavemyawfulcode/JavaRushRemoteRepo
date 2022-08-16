cd2995e7e1eb6f664eb4f4403b13e861bf94b1af
Justwantedtosavemyawfulcode <youjustalittlechicken@yandex.ru>
2022-08-16 18:36:06 +0300

    did something at home

--- 4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3309/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task33.task3309;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -23,7 +24,14 @@ import java.util.List;
 */
 
 public class Solution {
-    public static String toXmlWithComment(Object obj, String tagName, String comment) {
+    public static String toXmlWithComment(Object obj, String tagName, String comment) throws JAXBException {
+        StringWriter writer = new StringWriter();
+        JAXBContext context = JAXBContext.newInstance(obj.getClass());
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+        marshaller.marshal(obj, writer);
+        StringBuilder result = new StringBuilder(writer.toString());
+        int lastIndexOfEntry = result.lastIndexOf("<" + tagName + ">");
         return null;
     }
 
