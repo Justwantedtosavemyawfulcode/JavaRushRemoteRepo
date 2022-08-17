6d9b1e013d3bf83d35bba5e8dbfca6486d57e21a
Justwantedtosavemyawfulcode <youjustalittlechicken@yandex.ru>
2022-08-17 20:53:45 +0300

    did something at work

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Tetris.java
@@ -4,7 +4,17 @@ public class Tetris {
     private Field field;
     private Figure figure;
 
+    public Field getField() {
+        return field;
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
     public static void main(String[] args) {
         
     }
+
+
 }
