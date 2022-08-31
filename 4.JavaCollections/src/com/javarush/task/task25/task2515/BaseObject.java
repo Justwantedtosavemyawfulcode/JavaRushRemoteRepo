994de14f397470806d1104e5c657777af370ae64
Justwantedtosavemyawfulcode <youjustalittlechicken@yandex.ru>
2022-08-31 23:48:25 +0300

    did something at home

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/BaseObject.java
@@ -1,20 +1,22 @@
 package com.javarush.task.task25.task2515;
 
+/**
+ * Базовый класс для всех объектов игры.
+ */
 public abstract class BaseObject {
-    private double x;
-    private double y;
-    private double radius;
+    //координаты
+    protected double x;
+    protected double y;
+    //радиус объекта
+    protected double radius;
+    //состояние объект - жив ли объект
     private boolean isAlive;
 
     public BaseObject(double x, double y, double radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
-        isAlive = true;
-    }
-
-    public boolean isAlive() {
-        return isAlive;
+        this.isAlive = true;
     }
 
     public double getX() {
@@ -41,19 +43,50 @@ public abstract class BaseObject {
         this.radius = radius;
     }
 
-    public void move() {
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    public void draw(Canvas canvas) {
+        //do nothing
     }
 
-    public void draw() {
+    /**
+     * Двигаем себя на один ход.
+     */
+    public void move() {
+        //do nothing
+    }
 
+    /**
+     * Проверяем - не выходит ли (x,y) за границы.
+     */
+    public void checkBorders(double minx, double maxx, double miny, double maxy) {
+        if (x < minx) x = minx;
+        if (x > maxx) x = maxx;
+        if (y < miny) y = miny;
+        if (y > maxy) y = maxy;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
     }
 
     public void die() {
         isAlive = false;
     }
 
+    /**
+     * Проверяем - пересекаются ли переданный(o) и наш(this) объекты.
+     */
     public boolean isIntersect(BaseObject o) {
-        double distance = Math.sqrt(Math.pow((this.x - o.x), 2) + Math.pow((this.y - o.y), 2));
-        return distance < Math.max(this.radius, o.radius);
+        double dx = x - o.x;
+        double dy = y - o.y;
+        double distance = Math.sqrt(dx * dx + dy * dy);
+        double distance2 = Math.max(radius, o.radius);
+        return distance <= distance2;
     }
 }
